dbcebed570663e40aefd506b3ac9064d7296a829
gitlab <nikisazikov56@gmail.com>
2023-05-19 03:53:31 +0300

    first working version

--- tools/client.sh
+++ tools/client.sh
@@ -1,3 +1,3 @@
-g++ -std=c++17   ../src/main.cpp ../src/shell/*.cpp ../src/client/*.cpp   ../src/shell/user_cli/*.cpp  ../src/shell/server_cli/*.cpp  ../src/builders/*.cpp -lstdc++fs  -lboost_thread -lboost_timer -lboost_chrono -lrt
+g++ -std=c++17   ../src/main.cpp ../src/shell/*.cpp ../src/client/*.cpp   ../src/shell/user_cli/*.cpp  ../src/shell/server_cli/*.cpp  ../src/builders/*.cpp   ../src/parser/*.cpp -lstdc++fs  -lboost_thread -lboost_timer -lboost_chrono -lrt
 
 status=$?
